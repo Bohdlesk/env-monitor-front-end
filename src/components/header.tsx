1a702e50cbcaca7149b8c17f4d86e2f0502da13e
bohdlesk <bohdanleskovets@gmail.com>
2024-02-29 20:04:16 +0200

    Name in header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -11,7 +11,7 @@ const Header = () => {
                         <path stroke-linecap="round" stroke-linejoin="round"
                               d="M9 3v2m6-2v2M9 19v2m6-2v2M5 9H3m2 6H3m18-6h-2m2 6h-2M7 19h10a2 2 0 002-2V7a2 2 0 00-2-2H7a2 2 0 00-2 2v10a2 2 0 002 2zM9 9h6v6H9V9z"/>
                     </svg>
-                    <span className="ml-2 font-semibold text-[#252C32]">What a Market</span>
+                    <span className="ml-2 font-semibold text-[#252C32]">Enviro Monitor</span>
                 </div>
 
                 <div className="ml-2 flex">
